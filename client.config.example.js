b3f2da463720057b691e63ce532ae604d6bc2a30
Doan Toan <toan.doan@base.vn>
2025-06-20 11:36:30 +0700

    mirror update

--- client.config.example.js
+++ client.config.example.js
@@ -3,11 +3,13 @@ export default {
 		overlay: true, //show error on client
 		entryPoint: './public/index.html', // file inject code init hot reload
 		engine: 'default', // engine for resolve hot load file in server(/src/engine) and client(/public/engine)
+		injectCombine: false, // set true will combine all code hot load (init, client, engine) and inject to entry point, use when client cannot load file js from hot load server cuz of CPS policy
 	},
 	base: {
 		overlay: true,
-		entryPoint: '../data/base/{app}.cache/tcache/a.js.cch',
+		// entryPoint: '../data/base/{app}.cache/tcache/a.js.cch',
 		engine: 'base',
+		injectCombine: true
 	}
 }
 
